70be714b03b4cff0dd8ec89cd90a2b192b5ee150
Sahil Wasnik <04syee@gmail.com>
2024-03-24 09:18:05 +0530

    minor changes in etherscanapi

--- backend/src/api/utils/etherscanAPIUtil.ts
+++ backend/src/api/utils/etherscanAPIUtil.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getSoliditySourceCode(contractAddress: string) {
-  const url = `https://api-sepolia.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.ETHERSCAN_API_KEY}`;
+  const url = `https://etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${process.env.ETHERSCAN_API_KEY}`;
   const response = await axios.get(url);
 
   const contract = response.data.result[0].SourceCode
